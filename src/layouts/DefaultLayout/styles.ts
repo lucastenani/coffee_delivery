da8c8ef1de86b52e44b832eef78cf7744b0027d0
lucastenani <lucas.tenani3@gmail.com>
2023-09-29 01:42:40 -0300

    Creating a style for this layout to be followed across all routes

---
+++ src/layouts/DefaultLayout/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const LayoutContainer = styled.div`
+  max-width: 70rem;
+  margin: 0 auto;
+  padding: 0 1rem;
+
+  display: flex;
+  flex-direction: column;
+  gap: 2.5rem;
+`
