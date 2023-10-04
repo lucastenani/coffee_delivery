3db7dad3f402232c2703efbcbd84e466597b9d1d
lucastenani <lucas.tenani3@gmail.com>
2023-10-04 19:15:54 -0300

    Keeping only the message in the 'Selected Coffees' component

--- src/pages/Checkout/components/SelectedCoffees/index.tsx
+++ src/pages/Checkout/components/SelectedCoffees/index.tsx
@@ -2,25 +2,14 @@ import { Link } from 'react-router-dom'
 
 import EmptyCartImg from '../../../../assets/empty-cart.webp'
 
-import {
-  ConfirmOrderButton,
-  ConfirmOrderCard,
-  ConfirmOrderContainer,
-  EmptyCartMessage,
-} from './styles'
+import { EmptyCartMessage } from './styles'
 
 export function SelectedCoffees() {
   return (
-    <ConfirmOrderContainer>
-      <h2>Selected Coffees</h2>
-      <ConfirmOrderCard>
-        <EmptyCartMessage>
-          <img src={EmptyCartImg} alt="" />
-          <p>You haven not added any coffee to your cart yet</p>
-          <Link to={'/'}>Click here to continue shopping</Link>
-        </EmptyCartMessage>
-        <ConfirmOrderButton type="submit">Confirm Order</ConfirmOrderButton>
-      </ConfirmOrderCard>
-    </ConfirmOrderContainer>
+    <EmptyCartMessage>
+      <img src={EmptyCartImg} alt="" />
+      <p>You haven not added any coffee to your cart yet</p>
+      <Link to={'/'}>Click here to continue shopping</Link>
+    </EmptyCartMessage>
   )
 }
--- src/pages/Checkout/components/SelectedCoffees/styles.ts
+++ src/pages/Checkout/components/SelectedCoffees/styles.ts
@@ -1,16 +1,5 @@
 import styled from 'styled-components'
 
-import { baseCard } from '../../styles'
-
-export const ConfirmOrderContainer = styled.section`
-  max-width: 28rem;
-`
-
-export const ConfirmOrderCard = styled(baseCard)`
-  border-radius: 6px 44px;
-  gap: 2rem;
-`
-
 export const EmptyCartMessage = styled.div`
   display: flex;
   flex-direction: column;
@@ -30,29 +19,5 @@ export const EmptyCartMessage = styled.div`
     font-weight: 700;
     color: ${(props) => props.theme['primary-hover']};
     text-decoration: none;
-  }
-`
-
-export const ConfirmOrderButton = styled.button`
-  padding: 0.75rem 0.5rem;
-  border: 0;
-  border-radius: 6px;
-
-  font-size: 0.875rem;
-  font-weight: 700;
-  line-height: 160%;
-  text-transform: uppercase;
-
-  background: ${(props) => props.theme.primary};
-  color: ${(props) => props.theme.white};
-  cursor: pointer;
-
-  &:disabled {
-    opacity: 0.7;
-    cursor: not-allowed;
-  }
-
-  &:not(:disabled):hover {
-    background: ${(props) => props.theme['primary-hover']};
   }
 `
