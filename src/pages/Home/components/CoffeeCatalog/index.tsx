7905f6d2336ef60563ccf919d3ef6d2d1336146e
lucastenani <lucas.tenani3@gmail.com>
2023-10-05 21:04:19 -0300

    Styled the coffee catalog, but it's still static

--- src/pages/Home/components/CoffeeCatalog/index.tsx
+++ src/pages/Home/components/CoffeeCatalog/index.tsx
@@ -1,3 +1,54 @@
+import { ShoppingCart } from '@phosphor-icons/react'
+import traditionalCoffee from '../../../../assets/espresso.png'
+
+import {
+  AddToCart,
+  CoffeeAmount,
+  CoffeeBuy,
+  CoffeeCatalogContainer,
+  CoffeeDescription,
+  CoffeeImg,
+  CoffeeItem,
+  CoffeeMenu,
+  CoffeePrice,
+  CoffeeTag,
+  CoffeeTitle,
+} from './styles'
+
 export function CoffeeCatalog() {
-  return <h1>Coffee Catalog</h1>
+  return (
+    <CoffeeCatalogContainer>
+      <h3>Our Coffees</h3>
+
+      <CoffeeMenu>
+        <CoffeeItem>
+          <CoffeeImg src={traditionalCoffee} alt="" />
+
+          <CoffeeTag>TRADITIONAL</CoffeeTag>
+
+          <CoffeeTitle>Espresso</CoffeeTitle>
+
+          <CoffeeDescription>
+            The traditional coffee made with hot water and ground beans.
+          </CoffeeDescription>
+
+          <CoffeeBuy>
+            <CoffeePrice>
+              <span>$</span> 1.90
+            </CoffeePrice>
+
+            <CoffeeAmount>
+              <button id="decrement"> - </button>
+              <input type="number" min={1} max={15} value={1} readOnly />
+              <button id="increment"> + </button>
+            </CoffeeAmount>
+
+            <AddToCart>
+              <ShoppingCart weight="fill" size={22} />
+            </AddToCart>
+          </CoffeeBuy>
+        </CoffeeItem>
+      </CoffeeMenu>
+    </CoffeeCatalogContainer>
+  )
 }
