7e772e20dde6b43ba5e0d6bcc40833a77d444884
lucastenani <lucas.tenani3@gmail.com>
2023-10-11 17:08:08 -0300

    Now it's possible to see the item amount in the cart as they are added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
+import { useContext } from 'react'
 import { MapPin, ShoppingCartSimple } from '@phosphor-icons/react'
 import CoffeeLogo from '../../assets/logo.svg'
+import { CoffeesContext } from '../../contexts/CoffeesContext'
 
-import { CartContainer, HeaderContainer, LocationContainer } from './styles'
+import {
+  AmountOfItemsCart,
+  CartContainer,
+  HeaderContainer,
+  LocationContainer,
+} from './styles'
 import { NavLink } from 'react-router-dom'
 
 export function Header() {
+  const { coffeeCart } = useContext(CoffeesContext)
+  const amountOfCartItems = coffeeCart.length
+
   return (
     <HeaderContainer>
       <NavLink to="/">
@@ -19,6 +29,11 @@ export function Header() {
 
         <CartContainer to="/checkout" title="My Cart">
           <ShoppingCartSimple size={22} weight="fill" />
+          {amountOfCartItems > 0 ? (
+            <AmountOfItemsCart>{amountOfCartItems}</AmountOfItemsCart>
+          ) : (
+            ''
+          )}
         </CartContainer>
       </nav>
     </HeaderContainer>
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -36,4 +36,22 @@ export const CartContainer = styled(NavLink)`
   color: ${(props) => props.theme['primary-hover']};
   padding: 0.5rem;
   border-radius: 6px;
+  position: relative;
+`
+
+export const AmountOfItemsCart = styled.span`
+  background-color: ${(props) => props.theme['primary-hover']};
+  color: ${(props) => props.theme.white};
+  font-size: 0.75rem;
+  font-weight: 700;
+  text-align: center;
+
+  width: 1.25rem;
+  padding: 0.2rem;
+
+  border-radius: 50%;
+
+  position: absolute;
+  top: -0.5rem;
+  right: -0.5rem;
 `
